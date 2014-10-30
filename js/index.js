706a13b6f25b7af56685366d6014259de1fbf333
Akhil <akhil.madanan@nlindia.com>
2014-10-30 11:33:02 +0530

    as

--- js/index.js
+++ js/index.js
@@ -29,19 +29,9 @@ function onDeviceReady() {
         tx.executeSql('DROP TABLE IF EXISTS test_table');
         tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
 
-        // demonstrate PRAGMA:
-        db.executeSql("pragma table_info (test_table);", [], function(res) {
-            var node = document.createTextNode("PRAGMA res: " + JSON.stringify(res));
-            para.appendChild(node);
-            var element = document.getElementById("deviceready");
-            element.appendChild(para);
-            
-          console.log("PRAGMA res: " + JSON.stringify(res));
-        });
-
+       
         tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
-          console.log("insertId: " + res.insertId + " -- probably 1");
-          console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+         
             
              var node = document.createTextNode("insertId rowsAffected " + res.rowsAffected + " -- should be 1");
             para.appendChild(node);
@@ -54,7 +44,7 @@ function onDeviceReady() {
               console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
                 
                 
-            var node = document.createTextNode("res.rows.length: " + res.rows.length + " -- should be 1");
+            var node = document.createTextNode("res.rows.length: " + res.rows.length + " -- should be 1 and selected items" + res.rows.item(0).cnt);
             para.appendChild(node);
             var element = document.getElementById("deviceready");
             element.appendChild(para);
