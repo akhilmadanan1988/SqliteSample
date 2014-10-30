1179d66d63a104bf92378ac225b326a558ef0fcd
Akhil <akhil.madanan@nlindia.com>
2014-10-30 11:12:27 +0530

    zxczxc

--- js/index.js
+++ js/index.js
@@ -16,34 +16,62 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+var para = document.createElement("p");
+document.addEventListener("deviceready", onDeviceReady, false);
 
-        console.log('Received Event: ' + id);
+var node = document.createTextNode("PRAGMA res: " );
+            para.appendChild(node);
+            var element = document.getElementById("deviceready");
+            element.appendChild(para);
+function onDeviceReady() {
+  var db = window.sqlitePlugin.openDatabase({name: "DB"});
+    
+    
+ db.transaction(function(tx) {
+        tx.executeSql('DROP TABLE IF EXISTS test_table');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
+
+        // demonstrate PRAGMA:
+        db.executeSql("pragma table_info (test_table);", [], function(res) {
+            var node = document.createTextNode("PRAGMA res: " + JSON.stringify(res));
+            para.appendChild(node);
+            var element = document.getElementById("deviceready");
+            element.appendChild(para);
+            
+          console.log("PRAGMA res: " + JSON.stringify(res));
+        });
+
+        tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+          console.log("insertId: " + res.insertId + " -- probably 1");
+          console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+            
+              var node = document.createTextNode("rowsAffected " + res.rowsAffected + " -- should be 1");
+            para.appendChild(node);
+            var element = document.getElementById("deviceready");
+            element.appendChild(para);
+
+          db.transaction(function(tx) {
+            tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
+              console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+              console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+                
+                
+            var node = document.createTextNode("res.rows.length: " + res.rows.length + " -- should be 1");
+            para.appendChild(node);
+            var element = document.getElementById("deviceready");
+            element.appendChild(para);
+
+            });
+          });
+
+        }, function(e) {
+            
+             var node = document.createTextNode("ERROR: " + e.message);
+            para.appendChild(node);
+            var element = document.getElementById("deviceready");
+            element.appendChild(para);
+          console.log("ERROR: " + e.message);
+        });
+      });
     }
-};
